318dcdc6db4ac0160ab60f73d25f18669414918c
Ben Weisz <bencew99@gmail.com>
2023-01-06 11:39:31 -0500

    Ensure destructors are called before clean up

--- main.cpp
+++ main.cpp
@@ -14,23 +14,25 @@
 #include "graphics/Window.hpp"
 
 int main() {
-    int r = Window::InitWindow(640, 480, "Hello World");
-    if (r == -1)
-        return r;
+    {
+        int r = Window::InitWindow(640, 480, "Hello World");
+        if (r == -1)
+            return r;
 
-    Window::ClearColour = glm::vec3(66.f / 255.f, 133.f / 255.f, 166.f / 255.f);
+        Window::ClearColour = glm::vec3(66.f / 255.f, 133.f / 255.f, 166.f / 255.f);
 
-    Model model(Geometry::PLANE, "../res/base.mat");
-    Material* mat = model.GetMaterial();
-    mat->SetUniformVec3("uColour", glm::vec3(0, .5f, 0));
+        Model model(Geometry::PLANE, "../res/base.mat");
+        Material* mat = model.GetMaterial();
+        mat->SetUniformVec3("uColour", glm::vec3(0, .5f, 0));
 
-    while (!Window::ShouldClose()) {
-        /* Execute event results */
-        if (Input::IsPressed("esc"))
-            glfwSetWindowShouldClose(Window::Frame, GL_TRUE);
+        while (!Window::ShouldClose()) {
+            /* Execute event results */
+            if (Input::IsPressed("esc"))
+                glfwSetWindowShouldClose(Window::Frame, GL_TRUE);
 
-        model.Draw();
-        Window::Draw();
+            model.Draw();
+            Window::Draw();
+        }
     }
 
     Window::DestoryWindow();
